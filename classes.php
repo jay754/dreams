85d6df746ddfd3f589d65ed0cf8b83f69d9a896c
Jay Engineer <jayengineer6@gmail.com>
2013-06-24 13:49:53 -0400

    added the basic layout of the site

---
+++ classes.php
@@ -0,0 +1,40 @@
+<?php
+
+session_start();
+
+class lifegoals{
+
+	public $db = null;
+
+	function __construct(){
+		try {
+	    	$this->db = new PDO('mysql:host=localhost;dbname=beta', "root", "");
+	    	$this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		} catch(PDOException $e) {
+	    	echo 'ERROR: ' . $e->getMessage();
+		}
+	}
+
+	public function getUsers(){
+		$stmt = $this->db->prepare('SELECT * FROM users');
+		if($stmt->execute()){
+			$data = array();
+
+			while($row = $stmt->fetch()){$data[] = $row;}
+
+    		return $data;
+    	}
+    	else{
+    		return "error with getting the content";
+    	}
+	}
+
+	public function loggedin(){
+		if ((isset($_SESSION['name']))){
+			return True;
+		}
+	} 
+}
+
+$betaOBJ = new beta();
+?>
