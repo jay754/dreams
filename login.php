c644a73ea946dd6ac1252a397169153041b7a2fd
Jay Engineer <jayengineer6@gmail.com>
2014-04-12 17:19:28 -0400

    login whitespac

--- login.php
+++ login.php
@@ -31,25 +31,26 @@ if((isset($_POST["username"])&&isset($_POST["password"]))&&(!empty($_POST["usern
     $username = $_POST["username"];
     $password = $_POST["password"];
     try {
-	    $db = new PDO('mysql:host=localhost;dbname=beta', "root", "");
-	    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
-	    $data = $db->prepare("SELECT * FROM users WHERE FirstName = :username AND password = :password");
-	    $data->bindParam(":username", $username);
-		$data->bindParam(":password", $password);
-	    $data->execute();
-	    $result = $data->fetch(PDO::FETCH_ASSOC);
+        $db = new PDO('mysql:host=localhost;dbname=beta', "root", "");
+        $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+        $data = $db->prepare("SELECT * FROM users WHERE FirstName = :username AND password = :password");
+        $data->bindParam(":username", $username);
+        $data->bindParam(":password", $password);
+        $data->execute();
+        $result = $data->fetch(PDO::FETCH_ASSOC);
+	    
 	    if($data->execute()==1){
-	        $_SESSION["id"] = $result["P_id"];
-	        $_SESSION["name"] = $result["FirstName"];
+            $_SESSION["id"] = $result["P_id"];
+            $_SESSION["name"] = $result["FirstName"];
 
-	    	header('Location: index.php');
+            header('Location: index.php');
 	    }
 	    else{
 	    	print "wrong combo";
-	    }
-	} catch(PDOException $e) {
-	    echo 'ERROR: ' . $e->getMessage();
-	}
+        }
+    }catch(PDOException $e) {
+        print 'ERROR: ' . $e->getMessage();
+    }
 }
 
 ?>
